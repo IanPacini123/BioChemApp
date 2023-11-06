e215e43898245fdfcf5e5545033ff6be4e4d042f
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2023-11-06 14:33:44 -0300

    working roulette

---
+++ BioChemApp/BioChemApp/BioChemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioChemAppApp.swift
+//  BioChemApp
+//
+//  Created by Ian Pacini on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BioChemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
